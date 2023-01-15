1a252ebf692c511455e491cbe56ca0a7b3938bd0
dejano <dejano.with.tie@gmail.com>
2023-01-15 14:18:04 +0100

    fetch fingerprint from remote

--- oidc.tf
+++ oidc.tf
@@ -1,5 +1,10 @@
+# what are security implications here?
+data "tls_certificate" "github" {
+  url = "https://token.actions.githubusercontent.com/.well-known/openid-configuration"
+}
+
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [one(data.tls_certificate.github.certificates).sha1_fingerprint]
 }
