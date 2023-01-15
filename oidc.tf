90444ec64cde0e3f0abac6debe0629d35ae2f606
dejano <dejano.with.tie@gmail.com>
2023-01-15 14:26:18 +0100

    Fix thumbprint

--- oidc.tf
+++ oidc.tf
@@ -6,5 +6,5 @@ data "tls_certificate" "github" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github.certificates[*].sha1_fingerprint]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
