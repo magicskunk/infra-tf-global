e56f7685e28b38f4fc39c6efd786c1be9448ae66
dejano <dejano.with.tie@gmail.com>
2023-01-28 17:07:24 +0100

    Propagate AWS Console Profile Creation from us to eu region

--- event_bridge.tf
+++ event_bridge.tf
@@ -1,5 +1,5 @@
 data "aws_cloudwatch_event_bus" "default_us" {
-  provider = "aws.us"
+  provider = aws.us
   name     = "default"
 }
 
@@ -7,8 +7,49 @@ data "aws_cloudwatch_event_bus" "default" {
   name = "default"
 }
 
+data "aws_iam_policy_document" "event_bus_trust" {
+  provider = aws.us
+  statement {
+    sid     = ""
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+  }
+}
+
+data "aws_iam_policy_document" "allow_put_events" {
+  provider = aws.us
+  statement {
+    effect    = "Allow"
+    resources = [data.aws_cloudwatch_event_bus.default.arn]
+    actions   = ["events:PutEvents"]
+  }
+}
+
+resource "aws_iam_role" "invoke_event_bus" {
+  provider = aws.us
+  name = "event_bridge_invoke_event_bus"
+  assume_role_policy = data.aws_iam_policy_document.event_bus_trust.json
+}
+
+resource "aws_iam_policy" "allow_put_events" {
+  provider = aws.us
+  name = "allow_put_events"
+  policy = data.aws_iam_policy_document.allow_put_events.json
+}
+
+resource "aws_iam_role_policy_attachment" "invoke_event_bus" {
+  provider = aws.us
+  policy_arn = aws_iam_policy.allow_put_events.arn
+  role       = aws_iam_role.invoke_event_bus.name
+}
+
 resource "aws_cloudwatch_event_rule" "iam_login_profile_created_in_us" {
-  provider      = "aws.us"
+  provider      = aws.us
   name          = "capture_aws_iam_login_profile_created"
   description   = "Capture each AWS Console Profile Creation"
   event_pattern = <<EOF
@@ -23,27 +64,14 @@ resource "aws_cloudwatch_event_rule" "iam_login_profile_created_in_us" {
 EOF
 }
 
-data "aws_iam_policy_document" "allow_put_events" {
-  provider = "aws.us"
-  statement {
-    sid       = ""
-    effect    = "Allow"
-    resources = [data.aws_cloudwatch_event_bus.default.arn]
-    actions   = ["events:PutEvents"]
-  }
-}
-
-resource "aws_cloudwatch_event_bus_policy" "allow_put_events" {
-  provider       = "aws.us"
-  policy         = data.aws_iam_policy_document.allow_put_events.json
-  event_bus_name = data.aws_cloudwatch_event_bus.default_us.name
-}
-
 resource "aws_cloudwatch_event_target" "propagate_to_another_region" {
-  rule = aws_cloudwatch_event_rule.iam_login_profile_created_in_us.arn
-  arn  = aws_cloudwatch_event_rule.iam_login_profile_created.arn
+  provider = aws.us
+  role_arn = aws_iam_role.invoke_event_bus.arn
+  rule = aws_cloudwatch_event_rule.iam_login_profile_created_in_us.name
+  arn  = data.aws_cloudwatch_event_bus.default.arn
 }
 
+# invoke lambda
 resource "aws_cloudwatch_event_rule" "iam_login_profile_created" {
   name        = "capture_aws_iam_login_profile_created"
   description = "Capture each AWS Console Profile Creation"
